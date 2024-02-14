f800ced7291809482f22327b49a97c3b9d620127
Om Sharma <ombhatt1158@gmail.com>
2024-02-14 22:25:12 +0530

    try duplicate method1

---
+++ src/components/Duplicate.tsx
@@ -0,0 +1,13 @@
+export const Duplicate = () => {
+    return (
+
+        <div>   <button
+            type="button"
+            className="btn btn-success mt-3"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+        >
+            Job Post 1
+        </button></div>
+    )
+}
